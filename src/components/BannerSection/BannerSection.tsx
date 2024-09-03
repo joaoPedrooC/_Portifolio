ae201777071daceae1a8d666306165c1b049f9d8
João Pedro <jp871157@gmail.com>
2024-09-03 16:04:51 -0300

    fix: updated visual bug on banner section

--- src/components/BannerSection/BannerSection.tsx
+++ src/components/BannerSection/BannerSection.tsx
@@ -35,6 +35,7 @@ export const BannerSection = () => {
 
           <IoIosArrowDown className={`${styles.banner__arrow}`}/>
         </div>
+        
         <Suspense fallback={<Loader />}>
           <Avatar  />
         </Suspense>
