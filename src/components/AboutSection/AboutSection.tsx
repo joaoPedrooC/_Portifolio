d025b0aabe8919f2aa754c0a0e24c1b6f37ebc70
João Pedro <jp871157@gmail.com>
2024-09-21 17:58:17 -0300

    feat: added english texts on about section

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useRef, useState } from 'react'
 
 import styles from './styles.module.scss'
+import { useTranslation } from 'react-i18next'
 
 export const AboutSection = () => {
   const [visible, setVisible] = useState<boolean>(false)
   const aboutRef = useRef<HTMLParagraphElement>(null)
+
+  const { t } = useTranslation()
 
   const observer = new IntersectionObserver((entries) => entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -20,15 +23,10 @@ export const AboutSection = () => {
     <section className={styles.about__section}>
       <div className="container">
         <div>
-          <p className={`text text__big--1 ${visible ? 'animate__animated animate__fadeIn' : ''}`} ref={aboutRef}>Desenvolvedor web apaixonado pelo estudo em lógica de programação, iniciei meus estudos em C++ no fim do ano de 2021,
-          linguagem que estudo até hoje devido sua complexidade. No decorrer da minha jornada, participei de cursos e maratonas de 
-          programação que me ajudaram a desenvolver meu conhecimento. Hoje, estou cursando Ciência da Computação, com previsão 
-          de formatura para 2026.</p>
-          <p className={`text text__big--1 ${visible ? 'animate__animated animate__fadeIn' : ''}`}>Em 2024, iniciei meus estudos na Kenzie Academy Brasil, onde aprendi diversas tecnologias e soft skills. Também tive a
-          oportunidade de atuar como Monitor de Ensino Full Stack, auxiliando outros alunos do curso com suas atividades e entregas.
-          Desde então, tenho feito projetos pessoais como forma de praticar as tecnologias aprendidas e evoluir ainda mais!</p>
+          <p className={`text text__big--1 ${visible ? 'animate__animated animate__fadeIn' : ''}`} ref={aboutRef}>{t('about__text--1')}</p>
+          <p className={`text text__big--1 ${visible ? 'animate__animated animate__fadeIn' : ''}`}>{t('about__text--2')}</p>
         </div>
-        <button type="button" className={visible ? 'animate__animated animate__fadeInUp animate__delay-1s' : ''}><a href="" download className='text text__big--2'>Baixar currículo</a></button>
+        <button type="button" className={visible ? 'animate__animated animate__fadeInUp animate__delay-1s' : ''}><a href="" download className='text text__big--2'>{t('about__button')}</a></button>
       </div>
       <div className={styles.about__sectionBackground}></div>
     </section>
