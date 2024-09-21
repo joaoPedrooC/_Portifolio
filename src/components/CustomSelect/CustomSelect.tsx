e2360f4e9815926fb524e75e2a09d9f0897f7c6d
João Pedro <jp871157@gmail.com>
2024-09-21 16:56:42 -0300

    feat: added logic to change pages language

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -3,12 +3,15 @@ import styles from './styles.module.scss'
 
 import { GiBrazil } from "react-icons/gi";
 import { IoIosArrowDown } from "react-icons/io";
+import i18n from '../../i18n';
 
 export const CustomSelect = () => {
   const ref = useRef<HTMLDivElement>(null)
 
-  const changeInputValue = () => {
-    // const checkedInput = document.querySelector('input[type="radio"]:checked')
+  const changeInputValue = (e: any) => {
+    const lang: string = e.target.value
+
+    i18n.changeLanguage(lang.toLowerCase())
   }
 
   return (
@@ -17,10 +20,10 @@ export const CustomSelect = () => {
       <span className='text text__small--2'>PT</span>
       <IoIosArrowDown />
 
-        <form onInput={() => changeInputValue()} className={styles.select__optionsContainer}>
-          <input type="radio" name="languageValue" id="PT" value={"PT"} />
+        <form onInput={(e) => changeInputValue(e)} className={styles.select__optionsContainer}>
+          <input type="radio" name="languageValue" id="PT" value="pt" />
           <label htmlFor="PT" className='text text__small-1 font__black'>Português</label>
-          <input type="radio" name="languageValue" id="EN" value="EN" />
+          <input type="radio" name="languageValue" id="EN" value="en" />
           <label htmlFor="EN" className='text text__small-1 font__black'>English</label>
         </form>
       </div>
