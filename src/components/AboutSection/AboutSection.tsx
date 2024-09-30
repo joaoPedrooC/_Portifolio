0cc6adef6345ed78313824e7fdb1f0ca337fa2be
João Pedro <jp871157@gmail.com>
2024-09-30 07:49:11 -0300

    feat: added cv to download

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useRef, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import cv from '../../assets/pdf/cv-joao-pedro.pdf'
 
 import styles from './styles.module.scss'
-import { useTranslation } from 'react-i18next'
 
 export const AboutSection = () => {
   const [visible, setVisible] = useState<boolean>(false)
@@ -9,6 +11,7 @@ export const AboutSection = () => {
 
   const { t } = useTranslation()
 
+  console.log(document.querySelector('a.text.text__big--2'))
   const observer = new IntersectionObserver((entries) => entries.forEach((entry) => {
     if (entry.isIntersecting) {
       setVisible(true)
@@ -26,7 +29,7 @@ export const AboutSection = () => {
           <p className={`text text__big--1 ${visible ? 'animate__animated animate__fadeIn' : ''}`} ref={aboutRef}>{t('about__text--1')}</p>
           <p className={`text text__big--1 ${visible ? 'animate__animated animate__fadeIn' : ''}`}>{t('about__text--2')}</p>
         </div>
-        <button type="button" className={visible ? 'animate__animated animate__fadeInUp animate__delay-1s button__links' : ''}><a href="" download className='text text__big--2'>{t('about__button')}</a></button>
+        <button type="button" className={visible ? 'animate__animated animate__fadeInUp animate__delay-1s button__links' : ''}><a href={cv} download="CV - João Pedro" className='text text__big--2'>{t('about__button')}</a></button>
       </div>
     </section>
   )
